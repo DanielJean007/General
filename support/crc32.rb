ca0fe0552d518ccacd529775decdc701131d0212
Jeff Shantz <jsuwo@users.noreply.github.com>
2015-09-19 01:50:07 -0400

    Update crc32.rb

--- support/crc32.rb
+++ support/crc32.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# 6 lines of code to validate command line arguments, open and read a file, 
+# compute its CRC-32 checksum, and print it out.
+# 
+# Not really fair, is it? ;)
 require 'zlib'
 
 if ARGV.size != 1 || ! File.file?(ARGV[0])
